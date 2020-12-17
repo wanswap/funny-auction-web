1e4ec29c752e09812c30cef47791a571a9a95006
Morpheus <morpheus.blockchain@gmail.com>
2020-12-17 15:01:50 +0800

    update UI

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -79,7 +79,7 @@ function BasicLayout(props) {
     const getHistory = async ()=>{
       let ret = await sc.history(props.networkId, goodsToken)
       if (ret) {
-        ret = ret.reverse().slice(0, 5);
+        ret = ret.reverse().slice(0, 10);
         let historyData = [];
         for (let i=0; i<ret.length; i++) {
           historyData.push({
